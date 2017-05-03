dbac2b3374fcdb037ae62bd06a77cad0e66fb69f
lx950627 <liuxin1995627@163.com>
2017-05-03 23:45:25 +0200

    5.3 sql

--- DB.py
+++ DB.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 import pymongo
+import pymysql
 
 def MongoInsert(name,doc):
     mongo=MongoClient(host="localhost",port=27017)#establish a connection to specific host and port
@@ -11,3 +12,17 @@ def MongoInsert(name,doc):
         collection.save(item)
         #if exists and same, do nothing;if exists but different,update;if not exist, insert
     print("Insert Done")
+
+def MySQLInsert(doc):
+    db = pymysql.connect("localhost","root","1995627","miun")
+    cursor = db.cursor()# get cursor
+
+    sql = 'INSERT INTO courses (semester,code,title,program,startweek,endweek,credits,registration) ' \
+          'VALUES (%s,%s,%s,%s,%s,%s,%s,%s)'
+
+    for item in doc:
+        cursor.execute(sql,(item["semester"],item['code'],item['title'],item['program'],item['startweek'],item['endweek'],item['credits'],item['registration type']))
+
+    db.commit()
+    db.close()
+    print("Courses Insert Done")
